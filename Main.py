efc0c64a2ca972ae9655d9764b3b232e76b05976
Donnacha0111 <donnacha0111@gmail.com>
2025-09-21 11:54:25 +0100

    Added Class "Warrior"

--- Main.py
+++ Main.py
@@ -1,5 +1,12 @@
 def main():
-    print("Hello Mr Python")
+    class Warrior:
+        def __init__(War,health,strength,speed,species):
+         War.health = health
+         War.strength = strength
+         War.speed = speed
+         War.type = species
+         war1 = Warrior(65,45,5,"elf")
+         print(war1)
 
-    if __name__ == "__main__":
-        main()
+         if __name__ == "__main__":
+            main()
