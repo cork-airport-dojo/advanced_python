f39bc0f25f90627cd1943f8fb110dd57865c0d7f
Donnacha0111 <donnacha0111@gmail.com>
2025-09-21 11:58:26 +0100

    rewmoved class "Warrior"

--- Main.py
+++ Main.py
@@ -1,12 +1,4 @@
 def main():
-    class Warrior:
-        def __init__(War,health,strength,speed,species):
-         War.health = health
-         War.strength = strength
-         War.speed = speed
-         War.type = species
-         war1 = Warrior(65,45,5,"elf")
-         print(war1)
-
+ 
          if __name__ == "__main__":
             main()
